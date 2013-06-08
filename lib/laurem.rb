126657fb1c78c50afb1064b81a0cf5413980f97d
ohad partuck <ohadpartuck@gmail.com>
2013-06-08 21:20:22 +0300

    fixed methos

--- lib/laurem.rb
+++ lib/laurem.rb
@@ -3,21 +3,21 @@ require "tod"
 
 module Laurem
   def self.get_time
-    if TimeOfDay.new(8) < Time.now.to_time_of_day
+    if TimeOfDay.new(8) > Time.now.to_time_of_day
        puts "Damn,It early"
-    elsif TimeOfDay.new(10) < Time.now.to_time_of_day
+    elsif TimeOfDay.new(10) > Time.now.to_time_of_day
       puts "got your coffee yet?"
-    elsif TimeOfDay.new(12) < Time.now.to_time_of_day
+    elsif TimeOfDay.new(12) > Time.now.to_time_of_day
       puts "you should be waking up by now"
-    elsif TimeOfDay.new(13) < Time.now.to_time_of_day
+    elsif TimeOfDay.new(13) > Time.now.to_time_of_day
       puts "had launch yet?"
-    elsif TimeOfDay.new(16) < Time.now.to_time_of_day
+    elsif TimeOfDay.new(16) > Time.now.to_time_of_day
       puts "Do some work, lazy!"
-    elsif TimeOfDay.new(19) < Time.now.to_time_of_day
+    elsif TimeOfDay.new(19) > Time.now.to_time_of_day
       puts "get some dinner"
-    elsif TimeOfDay.new(22) < Time.now.to_time_of_day
+    elsif TimeOfDay.new(22) > Time.now.to_time_of_day
       puts "get a cold one let's talk about your day"
-    elsif TimeOfDay.new(23,59) < Time.now.to_time_of_day
+    elsif TimeOfDay.new(23,59) > Time.now.to_time_of_day
       puts "Getting a little late"
     end
   end
